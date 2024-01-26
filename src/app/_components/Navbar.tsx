17c4265034487f1bd846e7cf87f6727194e7b791
Seth Primack <sprimac2@gmail.com>
2024-01-26 09:15:52 -0600

    Add github action for nextjs

--- src/app/_components/Navbar.tsx
+++ src/app/_components/Navbar.tsx
@@ -2,8 +2,8 @@ import Link from "next/link"
 
 export default function Navbar() {
     return (
-        <nav className="navbar navbar-expand-lg bg-body-tertiary">
-            <div className="container-fluid">
+        <nav className="navbar navbar-expand bg-body-tertiary">
+            <div className="container-lg">
                 <a className="navbar-brand" href="/">Seth Primack</a>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
@@ -15,6 +15,16 @@ export default function Navbar() {
                         </li>
                         <li className="nav-item">
                             <Link href="/about" className="nav-link">About</Link>
+                        </li>
+                    </ul>
+                    <ul className="navbar-nav mb-2 mb-lg-0">
+                        <li className="nav-item">
+                            <Link href="/blog" className="nav-link">Blog</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link href="/blog" className="nav-link">Blog</Link>
+                        </li>                 <li className="nav-item">
+                            <Link href="/blog" className="nav-link">Blog</Link>
                         </li>
                     </ul>
                 </div>
